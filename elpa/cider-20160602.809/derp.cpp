9f9f625dd8f9db8f7f7eb1b255045b51b73639e8
slightlycyborg <slightlycyborg@gmail.com>
2016-10-26 04:35:38 +0000

    emacs.d

---
+++ elpa/cider-20160602.809/derp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int collin_age = 24;
+    cout << collin_age + 1;
+}
+
+// collin_age = 24
+// print(collin_age + 1)
---
+++ elpa/ipython-2927/ipython-autoloads.el
@@ -0,0 +1,18 @@
+;;; ipython-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "ipython" "ipython.el" (0 0 0 0))
+;;; Generated autoloads from ipython.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ipython" '("ipython-" "py-shell-initial-switch-buffers")))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; ipython-autoloads.el ends here
